aa4cd9f64393160ab3d3983601e1b21373803e74
Sanghak Kim <k3hppk@gmail.com>
2022-02-05 17:47:15 +0900

    Update: Add function that creates front page content
    why:
    * It's better to separate into the module that creates HTML content
    
    this commit:
    * creates a function that creates HTML content for the page for an user to place ships

---
+++ src/view/placingShipPage.ts
@@ -0,0 +1,60 @@
+import { Gameboard } from "../types";
+import { getXCoordChar } from "../utils/getXCoordChar";
+
+export function getPlacingShipPage(userName: string, gameBoard: Gameboard) {
+  const mainContainer = document.createElement("main");
+  mainContainer.className =
+    "bg-[#fff] flex flex-col grow items-center justify-center";
+
+  const gameBoardDisplay = createDisplayBoard(gameBoard);
+
+  const title = createTitle(userName);
+
+  mainContainer.append(title, gameBoardDisplay);
+
+  return mainContainer;
+}
+
+export function createDisplayBoard(gameBoard: Gameboard) {
+  const coordinates = gameBoard.getCoordinates();
+
+  const gameBoardContainer = document.createElement("div");
+  gameBoardContainer.className = `board w-[300px] h-[300px]`;
+
+  const cell = document.createElement("div");
+  cell.className = `cell w-[30px] h-full border-black border-[1px]`;
+
+  for (let i = 0; i < coordinates.length; i++) {
+    const row = document.createElement("div");
+    row.className = `row h-[30px] w-full flex`;
+
+    const coordinatesRow = coordinates[i];
+    for (let j = 0; j < coordinatesRow.length; j++) {
+      const gameBoardCell = coordinatesRow[j];
+      const cell = document.createElement("div");
+      cell.className = `cell w-[30px] h-full border-black border-[1px] flex items-center justify-center`;
+
+      if (gameBoardCell !== "noHit") {
+        cell.classList.add("bg-black", "text-white");
+        cell.innerText = gameBoardCell[0];
+      }
+
+      cell.dataset.xCoord = getXCoordChar(j);
+      cell.dataset.yCoord = String(i);
+
+      row.appendChild(cell);
+    }
+
+    gameBoardContainer.appendChild(row);
+  }
+
+  return gameBoardContainer;
+}
+
+function createTitle(userName: string) {
+  return document.createRange().createContextualFragment(`
+    <h2 class="text-center text-3xl uppercase font-bold">
+      Hey ${userName}, please place the ship!
+    </h2>
+  `);
+}
